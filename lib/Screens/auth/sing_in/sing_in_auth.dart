fcd4e5c32f4a2900b93a0e901b97c481b5febc35
Dima <dilmurodqurbonov2002@gmail.com>
2022-05-25 16:40:19 +0500

    Sing In  bilan Sing Up to'liq  bo'ldi

--- lib/Screens/auth/sing_in/sing_in_auth.dart
+++ lib/Screens/auth/sing_in/sing_in_auth.dart
@@ -1,106 +1,122 @@
 import 'package:flutter/material.dart';
 import 'package:med/core/constants/color_const.dart';
 
-class LogInPage extends StatelessWidget {
+class LogInPage extends StatefulWidget {
   const LogInPage({Key? key}) : super(key: key);
 
+  @override
+  State<LogInPage> createState() => _LogInPageState();
+}
+
+class _LogInPageState extends State<LogInPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: const Text("Log In")),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: const SizedBox(
-                child: Center(
-                  child: Text(
-                    "Log in to your account",
-                    style: TextStyle(fontSize: 22),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  child: Center(
+                    child: Text(
+                      "Log in to your account",
+                      style: TextStyle(fontSize: 22),
+                    ),
                   ),
                 ),
               ),
-            ),
-          const SizedBox(height: 73),
-                const Text("Phone nummber"),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      fillColor: Colors.grey,
-                      focusColor: Colors.grey,
-                      labelText: 'Phone',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(8.0),
-                        ),
+              const SizedBox(height: 73),
+              const Text("Phone nummber"),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(
+                    fillColor: Colors.grey,
+                    focusColor: Colors.grey,
+                    labelText: 'Phone',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8.0),
                       ),
                     ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Your password';
-                      }
-                      return null;
-                    },
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Your password';
+                    }
+                    return null;
+                  },
                 ),
-
-                const SizedBox(height: 22),
-                const Text("Your password"),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      fillColor: Colors.grey,
-                      focusColor: Colors.grey,
-                      labelText: 'password',
-                      suffixIcon: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.remove_red_eye_outlined),
-                      ),
-                      border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(8.0),
-                        ),
+              ),
+              const SizedBox(height: 22),
+              const Text("Your password"),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    fillColor: Colors.grey,
+                    focusColor: Colors.grey,
+                    labelText: 'password',
+                    suffixIcon: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.remove_red_eye_outlined),
+                    ),
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8.0),
                       ),
                     ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Your password';
-                      }
-                      return null;
-                    },
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Your password';
+                    }
+                    return null;
+                  },
                 ),
+              ),
               const SizedBox(height: 33),
-            Row(
-              children: [
-                const SizedBox(width: 14),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.078,
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  decoration: BoxDecoration(
-                    color: ColorConst.kBotton,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: TextButton(
-                    child: const Text(
-                      "Log In",
-                      style: TextStyle(
-                        color: ColorConst.white,
-                      ),
+              Row(
+                children: [
+                  const SizedBox(width: 14),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.078,
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    decoration: BoxDecoration(
+                      color: ColorConst.kBotton,
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, "/");
-                    },
+                    child: TextButton(
+                      child: const Text(
+                        "Log In",
+                        style: TextStyle(
+                          color: ColorConst.white,
+                        ),
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          String email = _nameController.text.trim();
+                          String password = _passwordController.text.trim();
+                          Navigator.pushNamed(context, "/");
+                        }
+                      },
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
--- lib/Screens/auth/sing_up/sing_up_auth.dart
+++ lib/Screens/auth/sing_up/sing_up_auth.dart
@@ -1,131 +1,149 @@
 import 'package:flutter/material.dart';
 import 'package:med/core/constants/color_const.dart';
 
-class SingUpPage extends StatelessWidget {
-  const SingUpPage({ Key? key }) : super(key: key);
+class SingUpPage extends StatefulWidget {
+  const SingUpPage({Key? key}) : super(key: key);
 
+  @override
+  State<SingUpPage> createState() => _SingUpPageState();
+}
+
+class _SingUpPageState extends State<SingUpPage> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _phoneController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: const Text("Sing Up")),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-         const   Padding(
-              padding:  EdgeInsets.all(8.0),
-              child:  SizedBox(
-                child: Center(
-                  child: Text(
-                    "Log in to your account",
-                    style: TextStyle(fontSize: 22),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: SizedBox(
+                  child: Center(
+                    child: Text(
+                      "Sign up in order to get a full access to the system",
+                      style: TextStyle(fontSize: 22),
+                    ),
                   ),
                 ),
               ),
-            ),
-          const SizedBox(height: 73),
-                const Text("Full name"),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      fillColor: Colors.grey,
-                      focusColor: Colors.grey,
-                      labelText: 'Name',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(8.0),
-                        ),
+              const SizedBox(height: 73),
+              const Text("Full name"),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(
+                    fillColor: Colors.grey,
+                    focusColor: Colors.grey,
+                    labelText: 'Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8.0),
                       ),
                     ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Your Name';
-                      }
-                      return null;
-                    },
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Your Name';
+                    }
+                    return null;
+                  },
                 ),
-                 const Text("Phone nummber"),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      fillColor: Colors.grey,
-                      focusColor: Colors.grey,
-                      labelText: 'Phone',
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(8.0),
-                        ),
+              ),
+              const Text("Phone nummber"),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    fillColor: Colors.grey,
+                    focusColor: Colors.grey,
+                    labelText: 'Phone',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8.0),
                       ),
                     ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Your nummber';
-                      }
-                      return null;
-                    },
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Your nummber';
+                    }
+                    return null;
+                  },
                 ),
-
-                const SizedBox(height: 22),
-                const Text("Your password"),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      fillColor: Colors.grey,
-                      focusColor: Colors.grey,
-                      labelText: 'password',
-                      suffixIcon: IconButton(
-                        onPressed: () {},
-                        icon: const Icon(Icons.remove_red_eye_outlined),
-                      ),
-                      border: const OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(8.0),
-                        ),
+              ),
+              const SizedBox(height: 22),
+              const Text("Your password"),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    fillColor: Colors.grey,
+                    focusColor: Colors.grey,
+                    labelText: 'password',
+                    suffixIcon: IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.remove_red_eye_outlined),
+                    ),
+                    border: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(8.0),
                       ),
                     ),
-                    validator: (value) {
-                      if (value!.isEmpty) {
-                        return 'Your password';
-                      }
-                      return null;
-                    },
                   ),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return 'Your password';
+                    }
+                    return null;
+                  },
                 ),
+              ),
               const SizedBox(height: 33),
-            Row(
-              children: [
-                const SizedBox(width: 14),
-                Container(
-                  height: MediaQuery.of(context).size.height * 0.078,
-                  width: MediaQuery.of(context).size.width * 0.9,
-                  decoration: BoxDecoration(
-                    color: ColorConst.kBotton,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: TextButton(
-                    child: const Text(
-                      "Log In",
-                      style: TextStyle(
-                        color: ColorConst.white,
-                      ),
+              Row(
+                children: [
+                  const SizedBox(width: 14),
+                  Container(
+                    height: MediaQuery.of(context).size.height * 0.078,
+                    width: MediaQuery.of(context).size.width * 0.9,
+                    decoration: BoxDecoration(
+                      color: ColorConst.kBotton,
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                    onPressed: () {
-                      Navigator.pushNamed(context, "/");
-                    },
+                    child: TextButton(
+                      child: const Text(
+                        "Log In",
+                        style: TextStyle(
+                          color: ColorConst.white,
+                        ),
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          String email = _nameController.text.trim();
+                          String password = _passwordController.text.trim();
+                          String phone = _phoneController.text.trim();
+                          Navigator.pushNamed(context, "/");
+                        }
+                      },
+                    ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
-
   }
 }
